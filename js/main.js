2a46d81c98f5193032ac06b83041d5867168ed69
Robert Warren <grindnmosh@gmail.com>
2013-06-07 00:31:15 -0700

    changed accordian to be built with jquery and css.

--- js/main.js
+++ js/main.js
@@ -19,33 +19,44 @@ $(document).ready(function(){
 	$('#add').on('pageinit', function() {
 		$('.input').on("focus", function() {
 			$(this).parent().addClass('highlight');
+			return false
 		});
-		
 		$('.input').on("blur", function(){
 			$(this).parent().removeClass('highlight')
+			return false
 		});
 		$("textarea").on("focus", function() {
 			$(this).parent().addClass('highlight');
+			return false
 		});
-		
 		$("textarea").on("blur", function(){
 			$(this).parent().removeClass('highlight')
+			return false
 		});
-		
 	});
 	
 	$('#view').on('pageinit', function() {
 		$("#clearAllData").on("click", function() {
-			localStorage.clear();	
+			localStorage.clear();
+			return false	
 		});
 	});
 		
 	$('#about').on('pageinit', function() {
-		
+		(function() {
+		$("dd").filter(":nth-child(n+4)").hide();
+		$("dt").on("click", function() {
+			$(this).next().fadeIn().siblings("dd").fadeOut();
+			return false	
+		})
+})();
 	});
 	
 	$('#err').on('pageinit', function() {
 		
 	});
+	
+
 
 });
+
